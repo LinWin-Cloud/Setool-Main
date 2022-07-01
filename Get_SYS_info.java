4133690b3e0aa29812590f1ca0f83f48db69c1b4
LinWin-Cloud <96580724+LinWin-Cloud@users.noreply.github.com>
2022-07-01 14:17:24 +0800

    Setool-Main

---
+++ Get_SYS_info.java
@@ -0,0 +1,192 @@
+import java.net.*;
+
+public class Get_SYS_info {
+    public static void main(String[]args)
+    {
+        /* get system information */
+        /* if use java run main function , then exit */
+        System.exit(0);
+    }
+    public static void Main_Get() throws Exception
+    {
+        /* first , write the log */
+        try
+        {
+            String ClassInfo = "Setool_RUN";
+            String log = "[GET_SYSINFO]Get system information";
+            String Error = "false" ;
+            log_server.log_print(ClassInfo, log, Error);
+        }
+        catch (Exception err)
+        {
+            System.out.println(err);
+        }
+        finally
+        {
+            //then get the system information
+            /* print the info in the console */
+            Thread.sleep(200);
+            for (int i = 1 ; i <= 1 ; i++)
+            {
+                System.out.println("");
+                System.out.println("==>ALL Information===>");
+                System.out.println("");
+            }
+            //print this host's net information
+            System.out.println("Host's NET information=>");
+            System.out.println("");
+            System.out.println("  [+]IP: "+Get_SYS_info.Get_IP());
+            System.out.println("  [+]Hostname: "+Get_SYS_info.Get_HOST());
+            Thread.sleep(200);
+            for (int i = 1 ; i <= 1 ; i++)
+            {
+                //print this host's system information
+                System.out.println("");
+                System.out.println("Your System info=>");
+                System.out.println("");
+                System.out.println("  [+]OS: "+Get_SYS_info.Get_OS());
+                System.out.println("  [+]Java Version: "+Get_SYS_info.Get_Java_Version());
+                System.out.println("  [+]WorkPath: "+Get_SYS_info.Get_WorkPath());
+                System.out.println("  [+]Users: "+Get_SYS_info.Get_Users());
+                System.out.println("  [+]RAM: "+Get_SYS_info.Get_RAM());
+                System.out.println("  [+]OS Arch: "+Get_SYS_info.OS_Arch());
+                System.out.println("  [+]OS Version: "+Get_SYS_info.OS_Version());
+                System.out.println("  [+]Users Home: "+Get_SYS_info.Get_Users_Home());
+            }
+            Thread.sleep(200);
+            for (int i = 1 ; i <= 1 ; i++)
+            {
+                //print this host's disk information
+                System.out.println("");
+                System.out.println("Your Disk info=>");
+                System.out.println("");
+                System.out.println("  [+]Disk: "+Get_SYS_info.Get_Disk());
+                System.out.println("  [+]Disk_Info: "+Get_SYS_info.Get_Disk_Info());
+            }
+            Thread.sleep(200);
+            for (int i = 1 ; i <= 1 ; i++)
+            {
+                //other information
+                System.out.println("");
+                System.out.println("Other info=>");
+                System.out.println("");
+                System.out.println("  [+]File Separator: "+Get_SYS_info.Get_File_Separator());
+                System.out.println("  [+]Line Separator: "+Get_SYS_info.Get_Line_Separator());
+                System.out.println("  [+]Path Separator: "+Get_SYS_info.Get_Path_Separator());
+                System.out.println("  [+]File Encoding: "+Get_SYS_info.Get_File_Encoding());
+                System.out.println("  [+]System Temp: "+Get_SYS_info.Get_System_Temp());
+            }
+            System.out.println("");
+        }
+    }
+    public static String Get_OS() throws Exception
+    {
+        //GET OS NAME
+        String OS = System.getProperty("os.name");
+        return OS;
+    }
+    public static String Get_IP() throws Exception
+    {
+        //GET IP ADDRESS
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        String IP = inetAddress.getHostAddress();
+        return IP;
+    }
+    public static String Get_HOST() throws Exception
+    {
+        //GET HOSTNAME
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        String Host = inetAddress.getHostName();
+        return Host;
+    }
+    public static String Get_Java_Version() 
+    throws Exception
+    {
+        //GET JAVA VERSION
+        String Java_Version = System.getProperty("java.version");
+        return Java_Version;
+    }
+    public static String Get_WorkPath() 
+    throws Exception
+    {
+        //GET WORKPATH
+        String WorkPath = System.getProperty("user.dir"); 
+        return WorkPath;
+    }
+    public static String Get_Users()
+    throws Exception
+    {
+        //GET USERS
+        String Users = System.getProperty("user.name");
+        return Users;
+    }
+    public static String Get_RAM()
+    throws Exception
+    {
+        //GET RAM
+        String RAM = System.getenv("PROCESSOR_IDENTIFIER");
+        return RAM;
+    }
+    public static String Get_Disk()
+    throws Exception
+    {
+        //GET DISK
+        String Disk = System.getenv("PROCESSOR_IDENTIFIER");
+        return Disk;
+    }
+    public static String Get_Disk_Info()
+    throws Exception
+    {
+        //GET DISK INFO
+        String Disk_Info = System.getenv("PROCESSOR_IDENTIFIER");
+        return Disk_Info;
+    }
+    public static String OS_Arch() throws Exception
+    {
+        //GET OS ARCH
+        String OS_Arch = System.getProperty("os.arch");
+        return OS_Arch;
+    }
+    public static String OS_Version() throws Exception
+    {
+        //GET OS VERSION
+        String OS_Version = System.getProperty("os.version");
+        return OS_Version;
+    }
+    public static String Get_File_Separator() throws Exception
+    {
+        //GET FILE SEPARATOR
+        String File_Separator = System.getProperty("file.separator");
+        return File_Separator;
+    }
+    public static String Get_Users_Home() throws Exception
+    {
+        //GET USERS HOME
+        String Users_Home = System.getProperty("user.home");
+        return Users_Home;
+    }
+    public static String Get_Line_Separator() throws Exception
+    {
+        //GET LINE SEPARATOR
+        String Line_Separator = System.getProperty("line.separator");
+        return Line_Separator;
+    }
+    public static String Get_Path_Separator() throws Exception
+    {
+        //GET PATH SEPARATOR
+        String Path_Separator = System.getProperty("path.separator");
+        return Path_Separator;
+    }
+    public static String Get_File_Encoding() throws Exception
+    {
+        //GET FILE ENCODING
+        String File_Encoding = System.getProperty("file.encoding");
+        return File_Encoding;
+    }
+    public static String Get_System_Temp() throws Exception
+    {
+        //GET SYSTEM TEMP
+        String System_Temp = System.getProperty("java.io.tmpdir");
+        return System_Temp;
+    }
+}
---
+++ function.java
@@ -0,0 +1,237 @@
+import java.io.File;
+import java.nio.file.Files;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+public class function 
+{
+    public static void CopyFiles(File resources , File target) 
+    throws Exception
+    {
+        //copy the file from the resources to the target
+        try
+        {
+            Files.copy(resources.toPath(), target.toPath());
+        }
+        catch(Exception e)
+        {
+            /* print the log to the log file */
+            String ClassInfo = "function_using";
+            String log = "Used Function Copyfiles";
+            String error = e.toString() ;
+            log_server.log_print(ClassInfo, log, error);
+            System.out.println("[!]Error: " + e);
+        }
+    }
+    public static void System_command(String command) throws Exception
+    {
+        //use system shell to run the command
+        String ClassInfo = "function_using";
+        String log = "Used System_command";
+        try
+        {
+            Runtime.getRuntime().exec(command);
+            String error = "true" ;
+            log_server.log_print(ClassInfo, log, error);
+        }
+        catch(Exception e)
+        {
+            String error = e.toString();
+            log_server.log_print(ClassInfo, log, error);
+            System.out.println("[!]Error: " + e);
+        }
+    }
+    public static void FileRead(File file)
+    {
+        /* first , write the log */
+        String ClassInfo = "function_using" ;
+        String log = "Used FileRead";
+        try
+        {
+            String error = "true" ;
+            log_server.log_print(ClassInfo, log, error);
+        }
+        catch (Exception ERR)
+        {
+            System.out.println(ERR);
+        }
+        //read the file form the target file
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+            br.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("[!]Error: " + e);
+        }
+    }
+    public static void LinuxFileCopy(File resources, File target) throws Exception
+    {
+        //copy the file from the source to the target (use linux shell)
+        String cmd = "cp " + resources + " " + target;
+        String execution = cmd;  
+        Process process = null;
+        BufferedReader input = null;
+        String user = "";
+
+        /* log */
+        String ClassInfo = "function_using";
+        String log = "Used Function:LinuxFileCopy";
+        try
+        {
+            /* write the log */
+            try
+            {
+                String error = "true" ;
+                log_server.log_print(ClassInfo, log, error);
+            }
+            catch (Exception err)
+            {
+                System.out.println(err);
+            }
+            process = Runtime.getRuntime().exec(execution);
+            process.waitFor();
+            int result = process.exitValue();     
+            input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            user = input.readLine();    
+            if (result == 1){
+                System.out.println("[!]Error: " + user);
+            }
+            else{
+                System.out.println("[+]Success: " + user);
+            }
+        }
+        catch (Exception e) 
+        {
+            String error = e.toString();
+            log_server.log_print(ClassInfo, log, error);
+        }
+        finally
+        {
+            //other optionings
+        }
+    }
+    public static void start_webbrowser(String URL) 
+    throws Exception
+    {
+        py_start_webbrowser(URL);
+    }
+    public static void py_start_webbrowser(String URL) throws Exception
+    {
+        /* The log info */
+        String ClassInfo = "function_using";
+        String log = "Used Function:py_start_webbrowser";
+        try
+        {
+            String error = "true" ;
+            log_server.log_print(ClassInfo, log, error);
+        }
+        catch (Exception err)
+        {
+            System.out.println(err);
+        }
+        try
+        {
+            String FilePath = "./URL_cfg.txt";
+            File file = new File(FilePath);
+            file.delete();
+            if (!file.exists())
+            {
+                //the file is not exists
+                file.createNewFile();
+                String content = URL;
+                String name = FilePath;
+                setool.wirte_file(content, name);
+            }
+            String command = "python3 python_code/start_webbrowser.py";
+            System_command(command);   
+        }
+        catch (Exception err)
+        {
+            System.out.println(err); //print the error information
+            String error = err.toString();
+            log_server.log_print(ClassInfo, log, error);
+        }
+    }
+    public static void LinuxWirteFile(String Path , String Content)
+    {
+        String command = "echo "+Content+" > "+Path;
+        exploit_social.shell1(command);
+    }
+    public static void Python_WriteFile() throws Exception {
+        try {
+            //... ...
+        } catch (Exception error) {
+            /* print the error log to the file */
+            System.out.println(error);
+            String ClassInfo = "function_using";
+            String log = "Use Function: Python_WriteFile";
+            String err = error.toString();
+            log_server.log_print(ClassInfo, log, err);
+        }
+    }
+    public static void Run_Python(String Py_Name) throws Exception
+    {
+        //use shell method
+        /* first , write the log */
+        try
+        {
+            String ClassInfo = "function_using" ;
+            String log = "Use Function:Run_Python";
+            String error = "false" ;
+            log_server.log_print(ClassInfo , log , error);
+        }
+        catch (Exception ERR)
+        {
+            System.out.println(ERR);
+        }
+        try
+        {
+            String run = "python3 ./python_code/"+Py_Name;
+            exploit_social.shell1(run);
+        }
+        catch (Exception err)
+        {
+            System.out.println(err);
+            String ClassInfo = "function_using" ;
+            String log = "Use Function:Run_Python";
+            String error = err.toString();
+            log_server.log_print(ClassInfo , log , error);
+        }
+    }
+    public static String Reture_Read(String in_file) throws Exception
+    {  
+        try (//read the file and return the content
+        BufferedReader reader = new BufferedReader(new FileReader(in_file))) {
+            String line;
+            while((line = reader.readLine()) != null)
+            {
+                return line;
+            }
+            reader.close();
+            return line;
+        }
+    }
+    public static void Wirte_Path(String str,String content) throws Exception
+    {
+        try
+        {
+            String NewName = str;
+            FileWriter fw=new FileWriter(NewName,true);
+            fw.write(content);
+            fw.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("[Error]:"+e.toString());
+        }
+    }
+}
