f466d2c13cc29092c1b001d4ccd86ba028e7c2d0
LinWin-Cloud <96580724+LinWin-Cloud@users.noreply.github.com>
2022-07-06 20:19:23 +0800

    Update function.java

--- function.java
+++ function.java
@@ -3,7 +3,7 @@ import java.nio.file.Files;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileWriter; 
 import java.io.InputStreamReader;
 public class function 
 {
